7d899743a0be74fdb28f922790985eb47f4abed0
Alejandro Seguí <alesegdia@gmail.com>
2015-04-08 22:12:13 +0200

    Dumb and ugly bot working.

--- Main.hx
+++ Main.hx
@@ -1,6 +1,7 @@
 
 import sys.net.Host;
 import sys.net.Socket;
+import haxe.io.Error;
 
 class Main {
 
@@ -17,21 +18,35 @@ class Main {
 		socket.connect(host, port);
 		socket.setFastSend(false);
 		socket.setTimeout(0);
+		socket.setBlocking(true);
 	}
 
 	private var host:Host;
 	private var socket:Socket = new Socket();
 
 	private function run():Void {
-		this.connect("irc.vieju.net", 6667);
-		this.send("NICK helix");
-		this.send("USER helix 8 * : helix");
+		this.connect("irc.freenode.net", 6667);
+		this.send("NICK helix__bot");
+		this.send("USER helix__bot 8 * : helix__bot");
 		this.send("JOIN " + "#asdf");
 		var exitLoop:Bool = false;
 		while(!exitLoop)
 		{
-			var msg:String = this.socket.read();
+			var msg:String = this.socket.input.readLine();
 			neko.Lib.println(msg);
+			var pingregex = ~/PING :.*/;
+			var commregex = ~/^:(.*)!.* ([A-Za-z0-9]*) (.*) :(.*)/i;
+			if( pingregex.match(msg) )
+			{
+				this.send("PONG");
+			}
+			else if( commregex.match(msg) )
+			{
+				neko.Lib.println("[" + commregex.matched(1) +
+						         " " + commregex.matched(2) +
+						      " -> " + commregex.matched(3) +
+						        "] " + commregex.matched(4) );
+			}
 		}
 	}
 
