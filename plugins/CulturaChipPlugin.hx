07b87c0a544570c6c86ece3fd1b28219cc49e80c
Alejandro Seguí <alesegdia@gmail.com>
2015-04-24 20:41:57 +0200

    Added 'last post' feature.

--- plugins/CulturaChipPlugin.hx
+++ plugins/CulturaChipPlugin.hx
@@ -13,7 +13,31 @@ class CulturaChipPlugin extends Plugin
 		var r = ~/!latest (topic|post)/;
 		if( r.match(message) ) {
 			if( r.matched(1) == "post" ) {
-				this.bot.Say("not implemented :3");
+				var data:String = Http.requestUrl("http://foro.culturachip.org/extern.php?action=posts_feed&type=rss");
+				var latest = SelectDom.select(TypedXml.parse(data), "item")[0];
+				trace(latest);
+				var post_title:String = SelectDom.select((latest), "title")[0].firstChild().toString();
+				var post_message:String = SelectDom.select((latest), "description")[0].firstChild().toString();
+				var post_author:String = SelectDom.select((latest), "author")[0].firstChild().toString();
+				trace(post_title);
+				r = ~/<!\[CDATA\[(.*)\]>/;
+				//r = new EReg("", "g");
+				if( r.match(post_title) ) {
+					post_title = r.matched(1);
+				}
+				r = ~/<!\[CDATA\[(.*)\]\]>/;
+				if( r.match(post_message) ) {
+					post_message = r.matched(1);
+				}
+				r = ~/<!\[CDATA\[ \((.*)\)\]\]>/;
+				if( r.match(post_author) ) {
+					post_author = r.matched(1);
+				}
+				var post_link = SelectDom.select(latest, "link")[0].firstChild();
+				trace(post_link);
+				trace(post_title);
+				this.bot.Say(sender + ", latest post by " + post_author + " at " + post_title + ": " + post_message.substr(0,200));
+				this.bot.Say(sender + ", more at " + post_link);
 			} else if( r.matched(1) == "topic" ) {
 				var data:String = Http.requestUrl("http://foro.culturachip.org/extern.php?action=feed&type=rss");
 				var latest = SelectDom.select(TypedXml.parse(data), "item")[0];
@@ -32,4 +56,5 @@ class CulturaChipPlugin extends Plugin
 			}
 		}
 	}
+
 }
